1eb063c2aee8987f4c1591941c5e71548581f1e3
tntaziz <153752116+tntaziz@users.noreply.github.com>
2023-12-13 20:56:59 +0500

    limit.py

---
+++ limit.py
@@ -0,0 +1,26 @@
+def get_max_limit(limit_lvl):
+    if limit_lvl == 1:
+        max_limit = 500000
+    elif limit_lvl == 2:
+        max_limit = 1000000
+    elif limit_lvl == 3:
+        max_limit = 1500000
+    elif limit_lvl == 4:
+        max_limit = 2000000
+    elif limit_lvl == 5:
+        max_limit = 2500000
+    elif limit_lvl == 6:
+        max_limit = 3000000
+    elif limit_lvl == 7:
+        max_limit = 3500000
+    elif limit_lvl == 8:
+        max_limit = 4000000
+    elif limit_lvl == 9:
+        max_limit = 4500000
+    elif limit_lvl == 10:
+        max_limit = 5000000
+    elif limit_lvl == 11:
+        max_limit = 5500000
+    elif limit_lvl == 12
+        max_limit = 6000000 
+  return max_limit
